5d9c143b572ee4285e9da753d557e682ae15e307
Joel Amaefula <joelamaefula@gmail.com>
2023-02-20 06:27:17 +0100

    made final adjustments to design

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 const sideBar = document.querySelector("#mobile_side_nav");
 const closeMenu = document.querySelector(".close-menu");
 const openMenu = document.querySelector(".open-menu");
+const dropdown = document.querySelectorAll(".dropdown");
 const dropdownBtns = document.querySelectorAll(".dropdown-btn");
+// const dropdownContent = document.querySelectorAll(".dropdown-content");
 
 openMenu.addEventListener("click", function () {
     sideBar.classList.add("active");
@@ -9,13 +11,30 @@ openMenu.addEventListener("click", function () {
 
 closeMenu.addEventListener("click", function () {
     sideBar.classList.remove("active");
+
+    dropdownHide();
 });
 
 dropdownBtns.forEach(function (dropdownBtn) {
     dropdownBtn.addEventListener("click", function(e){
         e.preventDefault();
-        // $(this).siblings(".dropdown").toggleClass("show");
-        this.nextElementSibling.classList.toggle("show");
+
+        // checks to know if the dropdown below it has been clicked
+        if (window.innerWidth >= 800) {
+            if (!this.parentElement.classList.contains("show")) {
+                dropdownHide();
+            }
+        }
+        
+        this.parentElement.classList.toggle("show");
+        // console.log(this.parentElement);
         
     });
 });
+
+function dropdownHide() {
+    // hides all the dropdown content
+    dropdown.forEach(function (item) {
+        item.classList.remove("show");
+    });
+}
